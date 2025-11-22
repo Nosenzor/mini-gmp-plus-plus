3460e36279e70d644e6ff7b744a52f850e3f901c
Nosenzor <romain_nosenzo@yahoo.fr>
2025-11-22 16:27:22 +0100

    Replace `BigInt` with `MiniMPZ` in `test_MiniMPZ.cpp`.

--- tests/test_MiniMPZ.cpp
+++ tests/test_MiniMPZ.cpp
@@ -1,16 +1,16 @@
 //
 // Created by Romain Nosenzo on 21/11/2025.
 //
-// test_BigInt.cpp
-#include "../BigInt.hpp"
+// test_MiniMPZ.cpp
+#include "../MiniMPZ.hpp"
 #include <iostream>
 #include <cassert>
 
 void test_construction() {
-    BigInt a(42ul);
-    BigInt b(3.14);
-    BigInt c(2.5f);
-    BigInt d("12345678901234567890");
+    MiniMPZ a(42ul);
+    MiniMPZ b(3.14);
+    MiniMPZ c(2.5f);
+    MiniMPZ d("12345678901234567890");
 
     assert(a.to_long() == 42);
     assert(b.to_long() == 3);
@@ -21,8 +21,8 @@ void test_construction() {
 }
 
 void test_arithmetic() {
-    BigInt a(100ul);
-    BigInt b(25ul);
+    MiniMPZ a(100ul);
+    MiniMPZ b(25ul);
 
     assert((a + b).to_long() == 125);
     assert((a - b).to_long() == 75);
@@ -35,30 +35,30 @@ void test_arithmetic() {
 }
 
 void test_compound_assignment() {
-    BigInt a(100ul);
+    MiniMPZ a(100ul);
 
-    a += BigInt(50ul);
+    a += MiniMPZ(50ul);
     assert(a.to_long() == 150);
 
-    a -= BigInt(30ul);
+    a -= MiniMPZ(30ul);
     assert(a.to_long() == 120);
 
-    a *= BigInt(2ul);
+    a *= MiniMPZ(2ul);
     assert(a.to_long() == 240);
 
-    a /= BigInt(4ul);
+    a /= MiniMPZ(4ul);
     assert(a.to_long() == 60);
 
-    a %= BigInt(17ul);
+    a %= MiniMPZ(17ul);
     assert(a.to_long() == 9);
 
     std::cout << "Compound assignment tests passed\n";
 }
 
 void test_comparison() {
-    BigInt a(100ul);
-    BigInt b(50ul);
-    BigInt c(100ul);
+    MiniMPZ a(100ul);
+    MiniMPZ b(50ul);
+    MiniMPZ c(100ul);
 
     assert(a > b);
     assert(b < a);
@@ -71,17 +71,17 @@ void test_comparison() {
 }
 
 void test_utilities() {
-    BigInt a(-42l);
+    MiniMPZ a(-42l);
     assert(a.abs().to_long() == 42);
     assert(a.sign() == -1);
 
-    BigInt b(2l);
+    MiniMPZ b(2l);
     assert(b.pow(10).to_long() == 1024);
 
-    BigInt c(16l);
+    MiniMPZ c(16l);
     assert(c.sqrt().to_long() == 4);
 
-    BigInt d(4l);
+    MiniMPZ d(4l);
     assert(d.is_even());
     assert(!d.is_odd());
 
@@ -89,18 +89,18 @@ void test_utilities() {
 }
 
 void test_large_numbers() {
-    BigInt a("123456789012345678901234567890");
-    BigInt b("987654321098765432109876543210");
+    MiniMPZ a("123456789012345678901234567890");
+    MiniMPZ b("987654321098765432109876543210");
 
-    BigInt sum = a + b;
+    MiniMPZ sum = a + b;
     assert(sum.to_string() == "1111111110111111111011111111100");
 
     std::cout << "Large number tests passed\n";
 }
 
 void test_float_construction() {
-    BigInt from_double(123.456);
-    BigInt from_float(78.9f);
+    MiniMPZ from_double(123.456);
+    MiniMPZ from_float(78.9f);
 
     assert(from_double.to_long() == 123);
     assert(from_float.to_long() == 78);
